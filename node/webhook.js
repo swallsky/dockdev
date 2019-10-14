aeeaf1a7c1a0d964073296f1a7b935c902227e27
swallsky <xjz1688@163.com>
2019-10-14 16:15:15 +0800

    webhook update

--- node/webhook.js
+++ node/webhook.js
@@ -39,6 +39,24 @@ const deployServer = http.createServer(function(request, response) {
       response.writeHead(200);
       response.end('Deploy Done.');
     });
+  }else if (request.url.search(/jymusic\/?$/i) > 0) {
+
+    let commands = [
+      'cd /var/www/jymusic',
+      'git pull origin master'
+    ].join(' && ');
+
+    exec(commands, (err, stdout, stderr)=> {
+      if (err) {
+        response.writeHead(500);
+        response.end('Server Internal Error.');
+        throw err;
+      }
+      // process.stderr.write(err);
+      // process.stdout.write(stdout);
+      response.writeHead(200);
+      response.end('Deploy Done.');
+    });
   } else {
     response.writeHead(404);
     response.end('Not Found.');
